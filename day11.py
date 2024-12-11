57c1d71ddc710349e0911ee41602d5d569583fc0
Alexander Balaban <balaban.alexander+github@gmail.com>
2024-12-11 06:06:37 +0000

    day11

---
+++ day11.py
@@ -0,0 +1,36 @@
+from collections import defaultdict 
+import utils 
+
+rows = utils.download_input(day=11)
+row = rows[0].split()
+pebbles = defaultdict(int)
+for pebble in row:
+    pebbles[pebble] += 1
+
+rules = [
+    lambda x: (True, ['1']) if x == '0' else (False, [x]), 
+    lambda x: (True, [x[:len(x) // 2], str(int(x[len(x) // 2:]))]) if len(x) % 2 == 0 else (False, [x]),
+    lambda x: (True, [str(int(x) * 2024)]), 
+]
+
+def blink(pebbles):
+    res = defaultdict(int)
+    for pebble, c in pebbles.items():
+        for rule in rules:
+            applied, m = rule(pebble)
+            if applied:
+                for p in m:
+                    res[p] += c
+                break
+    return res 
+
+
+def solve(pebbles, times):
+    for i in range(times):
+        pebbles = blink(pebbles)
+    return pebbles
+
+res1 = sum(solve(pebbles, 25).values())
+print(res1)
+res2 = sum(solve(pebbles, 75).values())
+print(res2)
